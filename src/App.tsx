17dd993800c87f2171472be5c5c4aea1fa02c3ea
VictorSB <v.sanchezbelmar@gmail.com>
2024-08-19 19:22:20 +0200

    feat: adding KYCViewer examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { useEffect, useState } from 'react';
-import {createKintoSDK, KintoAccountInfo } from 'kinto-web-sdk';
-import { encodeFunctionData, Address, getContract,
-  defineChain, createPublicClient, http } from 'viem';
+import { createKintoSDK, KintoAccountInfo } from 'kinto-web-sdk';
+import {
+  encodeFunctionData, Address, getContract,
+  defineChain, createPublicClient, http
+} from 'viem';
 import styled from 'styled-components';
 import AppHeader from 'components/shared/AppHeader';
 import AppFooter from 'components/shared/AppFooter';
-import { BaseScreen, BaseHeader, LearnLink, KintoAddress,
-  GlobalLoader, PrimaryButton } from 'components/shared';
+import {
+  BaseScreen, BaseHeader, LearnLink, KintoAddress,
+  GlobalLoader, PrimaryButton
+} from 'components/shared';
 import { BREAKPOINTS } from 'config';
 import { ReactComponent as CreditImage } from './credit.svg';
 import numeral from 'numeral';
+import contractsJSON from '../public/abis/7887.json';
 import './App.css';
 
-export const counterAbi = [{"type":"constructor","inputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"count","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"increment","inputs":[],"outputs":[],"stateMutability":"nonpayable"}];
+interface KYCViewerInfo {
+  isIndividual: boolean;
+  isCorporate: boolean;
+  isKYC: boolean;
+  isSanctionsSafe: boolean;
+  getCountry: string;
+  getWalletOwners: Address[];
+}
+
+export const counterAbi = [{ "type": "constructor", "inputs": [], "stateMutability": "nonpayable" }, { "type": "function", "name": "count", "inputs": [], "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }], "stateMutability": "view" }, { "type": "function", "name": "increment", "inputs": [], "outputs": [], "stateMutability": "nonpayable" }];
 
 const kinto = defineChain({
   id: 7887,
@@ -36,6 +50,7 @@ const kinto = defineChain({
 
 const KintoConnect = () => {
   const [accountInfo, setAccountInfo] = useState<KintoAccountInfo | undefined>(undefined);
+  const [kycViewerInfo, setKYCViewerInfo] = useState<any | undefined>(undefined);
   const [counter, setCounter] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const kintoSDK = createKintoSDK('0x14A1EC9b43c270a61cDD89B6CbdD985935D897fE');
@@ -80,6 +95,44 @@ const KintoConnect = () => {
     }
   }
 
+  async function fetchKYCViewerInfo() {
+    if (!accountInfo?.walletAddress) return;
+
+    const client = createPublicClient({
+      chain: kinto,
+      transport: http(),
+    });
+    const kycViewer = getContract({
+      address: contractsJSON.contracts.KYCViewer.address as Address,
+      abi: contractsJSON.contracts.KYCViewer.abi,
+      client: { public: client }
+    });
+
+    try {
+      const [isIndividual, isCorporate, isKYC, isSanctionsSafe, getCountry, getWalletOwners] = await Promise.all([
+        kycViewer.read.isIndividual([accountInfo.walletAddress]),
+        kycViewer.read.isCompany([accountInfo.walletAddress]),
+        kycViewer.read.isKYC([accountInfo.walletAddress]),
+        kycViewer.read.isSanctionsSafe([accountInfo.walletAddress]),
+        kycViewer.read.getCountry([accountInfo.walletAddress]),
+        kycViewer.read.getWalletOwners([accountInfo.walletAddress])
+      ]);
+
+      setKYCViewerInfo({
+        isIndividual,
+        isCorporate,
+        isKYC,
+        isSanctionsSafe,
+        getCountry,
+        getWalletOwners
+      } as KYCViewerInfo);
+    } catch (error) {
+      console.error('Failed to fetch KYC viewer info:', error);
+    }
+
+    console.log('KYCViewerInfo:', kycViewerInfo);
+  }
+
   async function fetchAccountInfo() {
     try {
       setAccountInfo(await kintoSDK.connect());
@@ -92,6 +145,12 @@ const KintoConnect = () => {
     fetchAccountInfo();
     fetchCounter();
   });
+
+  useEffect(() => {
+    if (accountInfo?.walletAddress) {
+      fetchKYCViewerInfo();
+    }
+  }, [accountInfo]);
 
   // todo: add info about the dev portal and link
   return (
@@ -136,6 +195,40 @@ const KintoConnect = () => {
                         <StyledMainAddress chainId={7887} address={accountInfo.appKey as Address} showExplorer showClipboard />
                       </WalletRowValue>
                     </WalletRow>
+                    {kycViewerInfo && (
+                      <>
+                        <WalletRow key="isIndividual">
+                          <WalletRowName>Is Individual</WalletRowName>
+                          <WalletRowValue>
+                            <ETHValue>{kycViewerInfo.isIndividual ? 'Yes' : 'No'}</ETHValue>
+                          </WalletRowValue>
+                        </WalletRow>
+                        <WalletRow key="isCorporate">
+                          <WalletRowName>Is Corporate</WalletRowName>
+                          <WalletRowValue>
+                            <ETHValue>{kycViewerInfo.isCorporate ? 'Yes' : 'No'}</ETHValue>
+                          </WalletRowValue>
+                        </WalletRow>
+                        <WalletRow key="isKYC">
+                          <WalletRowName>Is KYC</WalletRowName>
+                          <WalletRowValue>
+                            <ETHValue>{kycViewerInfo.isKYC ? 'Yes' : 'No'}</ETHValue>
+                          </WalletRowValue>
+                        </WalletRow>
+                        <WalletRow key="isSanctionsSafe">
+                          <WalletRowName>Is Sanctions Safe</WalletRowName>
+                          <WalletRowValue>
+                            <ETHValue>{kycViewerInfo.isSanctionsSafe ? 'Yes' : 'No'}</ETHValue>
+                          </WalletRowValue>
+                        </WalletRow>
+                        <WalletRow key="country">
+                          <WalletRowName>Country</WalletRowName>
+                          <WalletRowValue>
+                            <ETHValue>{kycViewerInfo.getCountry}</ETHValue>
+                          </WalletRowValue>
+                        </WalletRow>
+                      </>
+                    )}
                     <WalletRow key="counter">
                       <WalletRowName>Counter</WalletRowName>
                       <WalletRowValue>
@@ -143,9 +236,6 @@ const KintoConnect = () => {
                       </WalletRowValue>
                     </WalletRow>
                   </WalletRows>
-                  <WalletNotice>
-                    <span>Attention!</span> Only send funds to your wallet address in the Kinto Network
-                  </WalletNotice>
                   {accountInfo && (
                     <PrimaryButton onClick={increaseCounter}>
                       Increase Counter
@@ -244,7 +334,7 @@ const WalletRow = styled.div`
 `;
 
 const WalletRowName = styled.div`
-  width: 90px;
+  width: 150px;
   color: var(--night);
   font-size: 16px;
   font-weight: 700;
